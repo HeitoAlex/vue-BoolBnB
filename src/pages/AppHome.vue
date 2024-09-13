9081e7326034a438d101f87d5f4ea9100f97a87d
DanieleTrippodo <trippododaniele@gmail.com>
2024-09-13 16:05:09 +0200

    added layout of Footer

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,12 +1,14 @@
 <script>
     import HeaderMain from '../components/HeaderMain.vue';
+    import FooterMain from '../components/FooterMain.vue'
     import ProjectList from '../components/ProjectList.vue';
 
     export default {
 
         components:{
             ProjectList,
-            HeaderMain
+            HeaderMain,
+            FooterMain
         },
 
         data(){
@@ -20,6 +22,7 @@
 <template>
     <HeaderMain /> <!-- Testing -->
     <ProjectList/>
+    <FooterMain />
 </template>
 
 <style scoped>

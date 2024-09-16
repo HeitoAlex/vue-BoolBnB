be0a577b7e265ec03b486c9e4ec7731bd837933d
Luca Sammarco <luca.sammarco7@gmail.com>
2024-09-16 11:17:34 +0200

    fixed show single apartment

--- src/pages/AppApartment.vue
+++ src/pages/AppApartment.vue
@@ -1,46 +1,49 @@
 <script>
-    import SingleApartment from '../components/SingleApartment.vue';
-    import axios from 'axios';
-    
-    export default {
-        components:{
-            SingleApartment
-        },
+import SingleApartment from '../components/SingleApartment.vue';
+import axios from 'axios';
 
-        data(){
-            return{
-                apartment: null,
-            }
-        },
+export default {
+    components: {
+        SingleApartment
+    },
 
-        methods:{
-            getApartment(id){
-                axios.get(`http://127.0.0.1:8000/api/apartments/${id}`, {
-                    params: {
-                    
-                    }
-                })
-                .then((response) => {
-                    console.log(response.data.results);
-                    this.project = response.data.results;
-                })
-                .catch(function (error) {
-                    console.log(error);
-                })
-                
-            }
-        },
-
-        created(){
-            this.getApartment( this.$route.params.id );
+    data() {
+        return {
+            apartment: null,
         }
+    },
+
+   methods: {
+    getApartment(id) {
+        axios.get(`http://127.0.0.1:8000/api/apartments/${id}`)
+            .then((response) => {
+                console.log(response.data.result); // Controlla il risultato
+                this.apartment = response.data.result; // Usa `result` per accedere ai dati dell'appartamento
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
+},
+
+    created() {
+        this.getApartment(this.$route.params.id);
+    }
+}
 </script>
 
 <template>
     <div class="container">
         <div class="apartment-list">
-            <SingleApartment v-if=" apartment != null " :title="apartment.title" :rooms_num="apartment.rooms_num" :beds_num="apartment.beds_num" :bathroom_num="apartment.bathroom_num" :sq_mt="apartment.sq_mt" :address="apartment.address" :images="apartment.images" :visibility="apartment.visibility"/>
+            <SingleApartment v-if="apartment" 
+                :title="apartment.title" 
+                :rooms_num="apartment.rooms_num" 
+                :beds_num="apartment.beds_num" 
+                :bathroom_num="apartment.bathroom_num" 
+                :sq_mt="apartment.sq_mt" 
+                :address="apartment.address" 
+                :images="apartment.images" 
+                :visibility="apartment.visibility" />
         </div>
     </div>
 </template>

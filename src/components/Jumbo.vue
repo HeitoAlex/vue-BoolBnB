01b55acc2761b87648814562566ffc7f9c84c506
Samuele-Chioda <samuelechioda@gmail.com>
2024-09-16 19:54:07 +0200

    tolto style in eccesso

--- src/components/Jumbo.vue
+++ src/components/Jumbo.vue
@@ -28,33 +28,6 @@
     justify-content: center;
 }
 
-nav {
-    width: 100%;
-    position: absolute;
-    top: 0;
-    left: 0;
-    padding: 20px 8%;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-}
-
-nav .logo {
-    width: 80px;
-}
-
-nav ul li {
-    list-style: none;
-    display: inline-block;
-    margin-left: 40px;
-}
-
-nav ul li a {
-    text-decoration: none;
-    color: #fff;
-    font-size: 16px;
-}
-
 .content {
     text-align: center;
 }
@@ -69,17 +42,6 @@ nav ul li a {
 .content h1:hover {
     -webkit-text-stroke: 2px #fff;
     color: transparent;
-}
-
-.content a {
-    text-decoration: none;
-    display: inline-block;
-    color: #fff;
-    font-size: 24px;
-    border: 2px solid #fff;
-    padding: 14px 70px;
-    border-radius: 50px;
-    margin-top: 20px;
 }
 
 .back-video {

ab510e051ee1f94d428d141eed9db052705fbd08
DanieleTrippodo <trippododaniele@gmail.com>
2024-09-16 15:22:54 +0200

    Merge unite

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,15 +1,11 @@
 <script>
 
     import ApartmentList from '../components/ApartmentList.vue';
-    import HeaderMain from '../components/HeaderMain.vue';
-    import FooterMain from '../components/FooterMain.vue';
 
     export default {
 
         components:{
-            ApartmentList,
-            HeaderMain,
-            FooterMain
+            ApartmentList
         },
 
         data(){
@@ -21,9 +17,7 @@
 </script>
 
 <template>
-    <HeaderMain />
     <ApartmentList/>
-    <FooterMain />
 </template>
 
 <style scoped>
